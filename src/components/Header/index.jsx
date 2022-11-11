acc82a21fb37893cf517656fd59abfcdb7c495b4
Alexandre Mucarzel Fonseca <amfonseca98@gmail.com>
2022-11-11 20:08:23 -0300

    Started project

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,16 @@
+import './style.css'
+
+export function Header() {
+    return (
+        <div className='Header'>
+            <nav className='Navigation'>
+                <ul>
+                    <li>Home</li>
+                    <li>Agendamento</li>
+                    <li>Cadastro</li>
+                </ul>
+            </nav>
+        </div>
+    )
+  }
+  
