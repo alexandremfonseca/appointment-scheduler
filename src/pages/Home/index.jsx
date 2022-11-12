e520d72cb7a70c9296a766c5d7004bb0ce70b7b4
Alexandre Mucarzel Fonseca <amfonseca98@gmail.com>
2022-11-12 03:50:50 -0300

    Updated a few details

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,7 +6,7 @@ export function Home() {
     return (
         <div className="Home">
             <section className="welcomeMessage">
-                <h1>Seja bem-vindo à plataforma de agendamento!</h1>
+                <h1>Seja bem-vindo à plataforma de agendamento de consultas!</h1>
                 <p>Clique em <strong><Link to='/Appointments' className='Link'>Agendamento</Link></strong> para agendar sua consulta ou em <strong><Link to='/SignUp/Specialties' className='Link'>Cadastro</Link></strong> para cadastrar especialidades médicas, médicos ou pacientes!</p>
             </section>
         </div>
